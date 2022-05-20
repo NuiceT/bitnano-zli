bfff99f2d92095c10fc889ff5f0d8eb82d697c22
Leon Babic <leon@leonbabic.ch>
2022-05-20 10:19:24 +0200

    ✨ Add simple blockchain functionality

--- src/core/blockchain.ts
+++ src/core/blockchain.ts
@@ -11,4 +11,23 @@ export default class Blockchain {
     this.blockchain = [new Block(0, new Date().toUTCString(), [], "0")];
   }
 
+  public get latestBlock(): Block {
+    return this.blockchain[this.blockchain.length - 1];
+  }
+
+  public get valid(): boolean {
+    for (let i = 1; i < this.blockchain.length; i++) {
+      if (this.blockchain[i].hash !== this.blockchain[i].calculateHash())
+        return false;
+      if (this.blockchain[i].previousHash !== this.blockchain[i - 1].hash)
+        return false;
+    }
+    return true;
+  }
+
+  addBlock(block: Block): void {
+    block.previousHash = this.latestBlock.hash;
+    block.mineBlock(this.difficulty);
+    this.blockchain.push(block);
+  }
 }
