fa097fe59ed2004f3cbfd614a535f65ca0f197d1
Leon Babic <leon@leonbabic.ch>
2022-05-20 10:20:30 +0200

    ✅ Add Blockchain testing

---
+++ test/core/blockchain.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import Block from "../../src/core/block";
+import Blockchain from "../../src/core/blockchain";
+
+Blockchain.instance.addBlock(new Block(1, new Date().toUTCString(), [], ""));
+Blockchain.instance.addBlock(new Block(2, new Date().toUTCString(), [], ""));
+Blockchain.instance.addBlock(new Block(3, new Date().toUTCString(), [], ""));
+Blockchain.instance.addBlock(new Block(4, new Date().toUTCString(), [], ""));
+
+describe("⛓ Test Blockchain", (): void => {
+  it("✅ Verify valid Blockchain", (): void => {
+    expect(Blockchain.instance.valid).to.be.true;
+  });
+});
