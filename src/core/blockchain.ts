337446a8f7ae0326ba4cbd4e9dc1ae722dc05885
Leon Babic <leon@leonbabic.ch>
2022-06-02 14:07:05 +0200

    ✨ Add Transaction functionality

--- src/core/blockchain.ts
+++ src/core/blockchain.ts
@@ -1,4 +1,5 @@
 import Block from "./block";
+import Transaction from "./transaction";
 
 export default class Blockchain {
   public static instance = new Blockchain();
@@ -6,9 +7,15 @@ export default class Blockchain {
   private blockchain: Block[];
   private difficulty: number;
 
+  private pendingTransactions: Transaction[];
+
+  private reward: number;
+
   constructor() {
     this.difficulty = 2;
     this.blockchain = [new Block(0, new Date().toUTCString(), [], "0")];
+    this.pendingTransactions = [];
+    this.reward = 25;
   }
 
   public get latestBlock(): Block {
@@ -29,5 +36,37 @@ export default class Blockchain {
     block.previousHash = this.latestBlock.hash;
     block.mineBlock(this.difficulty);
     this.blockchain.push(block);
+  }
+
+  addTransaction(transaction: Transaction) {
+    this.pendingTransactions.push(transaction);
+  }
+
+  getBalance(address: string) {
+    let balance = 0;
+    this.blockchain.forEach((block: Block): void => {
+      block.transactions.forEach((transaction: Transaction): void => {
+        if (transaction.fromAddress === address) balance -= transaction.amount;
+        if (transaction.toAddress === address) balance += transaction.amount;
+      });
+    });
+    return balance;
+  }
+
+  mineTransactions(rewardAddress: string): void {
+    const block = new Block(
+      this.blockchain.length,
+      new Date().toUTCString(),
+      this.pendingTransactions,
+      this.latestBlock.hash
+    );
+
+    this.pendingTransactions.unshift(
+      new Transaction("", rewardAddress, this.reward)
+    );
+
+    block.mineBlock(this.difficulty);
+    this.blockchain.push(block);
+    this.pendingTransactions = [];
   }
 }
